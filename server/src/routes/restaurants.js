3874aff0ecfbb07ee0b82981cd82d688cae583c4
Jimmy Chi <jimmychi213@gmail.com>
2026-06-02 06:01:45 -0400

    Fix detail page to use images as photos

--- server/src/routes/restaurants.js
+++ server/src/routes/restaurants.js
@@ -329,7 +329,7 @@ router.get("/:id", async (req, res) => {
         const googleData = await getRestaurantDetails(atRest.placeId);
         restaurant = { ...googleData, ...atRest, name: atRest.name || googleData?.name, description: atRest.description || googleData?.description || null, address: atRest.address !== "Philadelphia, PA" ? atRest.address : googleData?.address || atRest.address, photos: googleData?.photos || [], reviews: googleData?.reviews || [], hours: googleData?.hours || null };
       } else {
-        restaurant = atRest;
+        restaurant = { ...atRest, photos: atRest.images && atRest.images.length > 0 ? atRest.images : atRest.image ? [atRest.image] : [] };
       }
     } else {
       restaurant = await getRestaurantDetails(id);
